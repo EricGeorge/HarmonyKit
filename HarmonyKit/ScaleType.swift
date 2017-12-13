6f9aa4c75dfbc4709ec6db6cbbb1c756b7b11701
Eric George <eegee.dev@gmail.com>
2017-12-13 13:23:39 -0800

    Added more scales to test with

--- HarmonyKit/ScaleType.swift
+++ HarmonyKit/ScaleType.swift
@@ -4,12 +4,22 @@ public enum ScaleType {
     case major
     case minor
     case harmonicMinor
+    case dorian
+    case phrygian
+    case lydian
+    case mixolydian
+    case locrian
     
     var intervals: [Interval] {
         switch self {
         case .major: return [.M2, .M3, .P4, .P5, .M6, .M7]
         case .minor: return [.M2, .m3, .P4, .P5, .m6, .m7]
         case .harmonicMinor: return [.M2, .m3, .P4, .P5, .m6, .M7]
+        case .dorian: return [.M2, .m3, .P4, .P5, .M6, .m7]
+        case .phrygian: return [.m2, .m3, .P4, .P5, .m6, .m7]
+        case .lydian: return [.M2, .M3, .D5, .P5, .M6, .M7]
+        case .mixolydian: return [.M2, .M3, .P4, .P5, .M6, .m7]
+        case .locrian: return [.m2, .m3, .P4, .D5, .m6, .m7]
         }
     }
 }
@@ -20,6 +30,11 @@ extension ScaleType : CustomStringConvertible {
         case .major: return "Major"
         case .minor: return "Minor"
         case .harmonicMinor: return "Harmonic Minor"
+        case .dorian: return "Dorian"
+        case .phrygian: return "Phrygian"
+        case .lydian: return "Lydian"
+        case .mixolydian: return "Mixolydian"
+        case .locrian: return "Locrian"
         }
     }
 }
--- Playgrounds/Scratchpad.playground/Contents.swift
+++ Playgrounds/Scratchpad.playground/Contents.swift
@@ -1,8 +1,8 @@
 import HarmonyKit
 
-let root = Note(.A, .flat, 5)
+let root = Note(.A, .sharp, 5)
 print("\(root) is MIDI value: \(root.midi)")
 
-let scale = Scale(root, .major)
+let scale = Scale(root, .dorian)
 print("The \(scale.root) \(scale.type) scale is \(scale.notes)")
 
